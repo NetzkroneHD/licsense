98b7e5213c0b49e13c440a2334d22e7665c83d09
NetzkroneHD <noah_01@gmx.de>
2024-03-04 15:06:49 +0100

    Private signature working

--- license-example-plugin/src/main/java/de/netzkronehd/licenseexample/LicensePlugin.java
+++ license-example-plugin/src/main/java/de/netzkronehd/licenseexample/LicensePlugin.java
@@ -1,7 +1,14 @@
 package de.netzkronehd.licenseexample;
 
+import de.netzkronehd.license.checker.siganture.SignatureChecker;
 import de.netzkronehd.licenseexample.checker.ScheduledChecker;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public final class LicensePlugin extends JavaPlugin {
 
@@ -9,7 +16,12 @@ public final class LicensePlugin extends JavaPlugin {
     public void onEnable() {
 
         final String key = getConfig().getString("license-key");
-        final ScheduledChecker scheduledChecker = new ScheduledChecker(this, "http://localhost:8080");
+        final ScheduledChecker scheduledChecker;
+        try {
+            scheduledChecker = new ScheduledChecker(this, "http://localhost:8080/license/api/v1", new SignatureChecker());
+        } catch (URISyntaxException | IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
         scheduledChecker.startChecking(key);
 
 
