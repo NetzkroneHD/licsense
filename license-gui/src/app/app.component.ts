7284dc5e12cd400b497fdebe88caab81c66c99dc
Noah <noah_01@gmx.de>
2024-12-16 13:36:42 +0100

    material 18 -> 19

--- license-gui/src/app/app.component.ts
+++ license-gui/src/app/app.component.ts
@@ -5,9 +5,13 @@ import {MatIconButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {TranslateModule, TranslateService} from '@ngx-translate/core';
-import {LicenseDropdownMenuComponent} from './component/license-dropdown-menu/license-dropdown-menu.component';
+import {
+    LicenseDropdownMenuComponent
+} from './component/license-dropdown-menu/license-dropdown-menu.component';
 import {MatMenuTrigger} from '@angular/material/menu';
-import {LicenseDropdownMenuItem} from './component/license-dropdown-menu/license-dropdown-menu-item.interface';
+import {
+    LicenseDropdownMenuItem
+} from './component/license-dropdown-menu/license-dropdown-menu-item.interface';
 import {UserSettingsStoreFacade} from './state/user-settings/user-settings-store-facade.service';
 import {LicenseSidenavComponent} from './component/license-sidenav/license-sidenav.component';
 import {LicenseSidenavItem} from './component/license-sidenav/license-sidenav-item.interface';
@@ -34,72 +38,72 @@ import {UserSettingsStore} from './state/user-settings/user-settings-store.servi
 })
 export class AppComponent implements OnInit {
 
-  protected readonly title = environment.title;
+    protected readonly title = environment.title;
 
-  @ViewChild('sidenav') sidenav!: LicenseSidenavComponent;
+    @ViewChild('sidenav') sidenav!: LicenseSidenavComponent;
 
-  private readonly userSettingsFacade = inject(UserSettingsStoreFacade);
-  private readonly userSettingsStore = inject(UserSettingsStore);
-  private readonly routeStoreService = inject(RouteStoreFacade);
-  private readonly routeStore = inject(RouteStore);
-  private readonly translateService = inject(TranslateService);
-  private readonly loginService = inject(LoginService);
+    private readonly userSettingsFacade = inject(UserSettingsStoreFacade);
+    private readonly userSettingsStore = inject(UserSettingsStore);
+    private readonly routeStoreService = inject(RouteStoreFacade);
+    private readonly routeStore = inject(RouteStore);
+    private readonly translateService = inject(TranslateService);
+    private readonly loginService = inject(LoginService);
 
-  constructor() {
+    constructor() {
 
-    effect(() => {
-      const route = this.routeStore.getCurrentRoute();
-      this.clearToggle();
-      uiItems.sidenavItems.filter(item => item.id === route).forEach(item => {
-        item.selected = true;
-      });
-    });
+        effect(() => {
+            const route = this.routeStore.getCurrentRoute();
+            this.clearToggle();
+            uiItems.sidenavItems.filter(item => item.id === route).forEach(item => {
+                item.selected = true;
+            });
+        });
 
-    effect(() => {
-      this.userSettingsStore.getUserLanguage();
-      uiItems.sidenavItems.forEach(item => {
-        item.description = this.translateService.instant('sidenavItems.' + item.id);
-      });
-      uiItems.homeContextMenuItems.forEach(item => {
-        item.title = this.translateService.instant('home.contextMenuItems.' + item.id);
-      });
-      uiItems.logoutDropdownMenu.forEach(item => {
-        if (!item.title) return;
-        item.title = this.translateService.instant(item.id);
-      })
-    });
-  }
-
-  ngOnInit() {
-    this.loginService.login();
-  }
-
-  protected onChangeLanguage(item: LicenseDropdownMenuItem) {
-    this.userSettingsFacade.changeLanguage(item.id);
-  }
-
-  protected onLogoutClicked(item: LicenseDropdownMenuItem) {
-    if (item.id !== 'logout') return;
-    this.loginService.logout();
-  }
-
-  protected toggleSidenav() {
-    this.sidenav.toggle();
-  }
-
-  protected sidenavItemClick(item: LicenseSidenavItem) {
-    if (item.id === 'settings') {
-      window.open(environment.accountSettingsUrl);
-      return;
+        effect(() => {
+            this.userSettingsStore.getUserLanguage();
+            uiItems.sidenavItems.forEach(item => {
+                item.description = this.translateService.instant('sidenavItems.' + item.id);
+            });
+            uiItems.homeContextMenuItems.forEach(item => {
+                item.title = this.translateService.instant('home.contextMenuItems.' + item.id);
+            });
+            uiItems.logoutDropdownMenu.forEach(item => {
+                if (!item.title) return;
+                item.title = this.translateService.instant(item.id);
+            })
+        });
     }
-    this.clearToggle();
-    item.selected = true;
-    this.routeStoreService.setCurrentRoute(toRoute(item.id));
-  }
 
-  private clearToggle() {
-    uiItems.sidenavItems.forEach(value => value.selected = false);
-  }
+    ngOnInit() {
+        this.loginService.login();
+    }
 
-  protected readonly uiItems = uiItems;
+    protected onChangeLanguage(item: LicenseDropdownMenuItem) {
+        this.userSettingsFacade.changeLanguage(item.id);
+    }
+
+    protected onLogoutClicked(item: LicenseDropdownMenuItem) {
+        if (item.id !== 'logout') return;
+        this.loginService.logout();
+    }
+
+    protected toggleSidenav() {
+        this.sidenav.toggle();
+    }
+
+    protected sidenavItemClick(item: LicenseSidenavItem) {
+        if (item.id === 'settings') {
+            window.open(environment.accountSettingsUrl);
+            return;
+        }
+        this.clearToggle();
+        item.selected = true;
+        this.routeStoreService.setCurrentRoute(toRoute(item.id));
+    }
+
+    private clearToggle() {
+        uiItems.sidenavItems.forEach(value => value.selected = false);
+    }
+
+    protected readonly uiItems = uiItems;
 }
--- license-gui/src/app/component/license-choice/license-choice.component.spec.ts
+++ license-gui/src/app/component/license-choice/license-choice.component.spec.ts
@@ -1,18 +1,18 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {LicenseChoiceMenuComponent} from './license-choice.component';
 
-import {LicenseChoiceComponent} from './license-choice.component';
 
 describe('LicenseInputComponent', () => {
-  let component: LicenseChoiceComponent;
-  let fixture: ComponentFixture<LicenseChoiceComponent>;
+  let component: LicenseChoiceMenuComponent;
+  let fixture: ComponentFixture<LicenseChoiceMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LicenseChoiceComponent]
+      imports: [LicenseChoiceMenuComponent]
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(LicenseChoiceComponent);
+    fixture = TestBed.createComponent(LicenseChoiceMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
