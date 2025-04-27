61931726dcead039e33a44f004bc6776cff771b2
Noah <noah_01@gmx.de>
2025-04-27 10:24:56 +0200

    refactor: remove unused download methods in file service

--- license-gui/src/app/service/file.service.ts
+++ license-gui/src/app/service/file.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import * as forge from 'node-forge';
 
 @Injectable({
@@ -7,34 +6,13 @@ import * as forge from 'node-forge';
 })
 export class FileService {
 
-    constructor(private http: HttpClient) {
-    }
-
-    public downloadFile(filename: string): void {
-        this.http.get(`assets/${filename}`, {responseType: 'blob'})
-            .subscribe((data: Blob) => {
-                const blob = new Blob([data], {type: 'application/octet-stream'});
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = filename;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-            });
-    }
-
-    public downloadPublicKey(): void {
-        this.downloadFile('license-key-public.der');
-    }
-
     public downloadGeneratedPublicKey(publicKeyPem: string): void {
         // Convert PEM to DER
         const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
         const derBuffer = forge.asn1.toDer(forge.pki.publicKeyToAsn1(publicKey)).getBytes();
 
         // Create a Blob from the DER buffer
-        const blob = new Blob([new Uint8Array(derBuffer.split('').map(char => char.charCodeAt(0)))], { type: 'application/x-x509-ca-cert' });
+        const blob = new Blob([new Uint8Array(derBuffer.split('').map(char => char.charCodeAt(0)))], {type: 'application/x-x509-ca-cert'});
 
         // Create a URL for the Blob and trigger the download
         const url = window.URL.createObjectURL(blob);
@@ -45,5 +23,4 @@ export class FileService {
         a.click();
         window.URL.revokeObjectURL(url);
     }
-
 }
