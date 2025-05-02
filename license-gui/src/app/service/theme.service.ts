fe16fd850462c574b46dcef783b2a345f95d4c0b
Noah <noah_01@gmx.de>
2025-05-02 03:17:03 +0200

    theming switching works again

--- license-gui/src/app/service/theme.service.ts
+++ license-gui/src/app/service/theme.service.ts
@@ -12,8 +12,41 @@ export class ThemeService {
     }
 
     public changeTheme(theme: Theme) {
-        const htmlElement = document.documentElement;
-        htmlElement.classList.remove('light-theme', 'dark-theme');
-        htmlElement.classList.add(theme);
+        this.removeStyle('theme');
+        this.setStyle('theme', `${theme}.css`);
     }
+
+    /**
+     * Set the stylesheet with the specified key.
+     */
+    setStyle(key: string, href: string) {
+        getLinkElementForKey(key).setAttribute('href', href);
+    }
+
+    /**
+     * Remove the stylesheet with the specified key.
+     */
+    removeStyle(key: string) {
+        getExistingLinkElementByKey(key)?.remove();
+    }
+}
+
+function getLinkElementForKey(key: string) {
+    return getExistingLinkElementByKey(key) || createLinkElementWithKey(key);
+}
+
+function getExistingLinkElementByKey(key: string) {
+    return document.head.querySelector(`link[rel="stylesheet"].${getClassNameForKey(key)}`);
+}
+
+function createLinkElementWithKey(key: string) {
+    const linkEl = document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.classList.add(getClassNameForKey(key));
+    document.head.appendChild(linkEl);
+    return linkEl;
+}
+
+function getClassNameForKey(key: string) {
+    return `style-manager-${key}`;
 }
